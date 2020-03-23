4e6f6f3c2ffa8135062130ea8f6a5839685f0559
deansher <deansherthompson@gmail.com>
2020-03-23 10:16:25 -0400

    progress on the basics of JoyData

--- src/me/joypri/JoyData.kt
+++ src/me/joypri/JoyData.kt
@@ -1,27 +1,32 @@
 package me.joypri
 
-import kotlin.reflect.KClass
+open class JoyData private constructor(val valueByQualifiedName: Map<String, Any>) {
 
-open class JoyData private constructor(val data: Map<Key<*>, Any>) {
+    constructor(vararg entries: KeyValue) :
+            this(HashMap<String, Any>().apply {
+                for (entry in entries) {
+                    this[entry.keyName] = entry.value
+                }
+            })
 
-}
-
-open class Key<V : Any>(val valueClass: KClass<V>) {
-    val qualifiedName: String =
-        (this::class.qualifiedName
-            ?: throw IllegalStateException("DataKey reiclass must have a qualifiedName: ${this::class.qualifiedName}"))
-
-    fun getFrom(data: Map<String, Any>): V? {
-        val anyValue = data[qualifiedName]
-        if (anyValue == null) {
-            return null
-        } else if (!valueClass.isInstance(anyValue)) {
-            throw IllegalStateException(
-                "Data type for $qualifiedName should be $valueClass; instead is ${anyValue::class}"
-            )
-        } else {
-            @Suppress("UNCHECKED_CAST")
-            return anyValue as V
+    inline operator fun <reified V> get(key: Key<V>): V? {
+        return when(val value = this.valueByQualifiedName[key.qualifiedName]) {
+            null -> null
+            is V -> value
+            else -> throw IllegalStateException(
+                "Expected data[${key.qualifiedName}] to have a ${V::class}, but there's a ${value::class}")
         }
     }
+}
+
+open class Key<V> {
+    val qualifiedName: String =
+        (this::class.qualifiedName
+            ?: throw IllegalStateException("DataKey class must have a qualifiedName; it is null'"))
+}
+
+data class KeyValue internal constructor(val keyName: String, val value: Any)
+
+fun <V: Any> Key<V>.to(value: V): KeyValue {
+    return KeyValue(qualifiedName, value)
 }
--- test/me/joypri/JoyDataSpec.kt
+++ test/me/joypri/JoyDataSpec.kt
@@ -4,15 +4,20 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
 
-object FirstName: Key<String>(String::class)
+object FirstName: Key<String>()
+object Age: Key<Int>()
 
 object JoyDataSpec : Spek({
     describe("Key") {
         it("has the right fully qualified name") {
             assertEquals("me.joypri.FirstName", FirstName.qualifiedName)
         }
-        it("has its declared valueClass") {
-            assertEquals(String::class, FirstName.valueClass)
+    }
+    describe("JoyData") {
+        it("constructs and gets") {
+            val data = JoyData(FirstName.to("Fred"), Age.to(12))
+            assertEquals("Fred", data[FirstName])
+            assertEquals(12, data[Age])
         }
     }
 })
