b90847105834d2a061418a5dfaf8cf3a00fd3365
deansher <deansherthompson@gmail.com>
2020-06-13 09:47:47 -0400

    Introduced mapAt(RolePath, ...)

--- src/main/kotlin/me/joypri/Mix.kt
+++ src/main/kotlin/me/joypri/Mix.kt
@@ -6,12 +6,12 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 import kotlin.reflect.full.primaryConstructor
 
-interface MixData {
+interface MixParts {
     val valueByQualifiedName: Map<String, Any>
     val parts: List<Part> get() = valueByQualifiedName.map { Part(it.key, it.value) }
 }
 
-inline operator fun <M : MixData, reified V> M.get(role: Role<V>): V? {
+inline operator fun <M : MixParts, reified V> M.get(role: Role<V>): V? {
     return when (val value = this.valueByQualifiedName[role.qualifiedName]) {
         null -> null
         is V -> value
@@ -23,32 +23,95 @@ inline operator fun <M : MixData, reified V> M.get(role: Role<V>): V? {
 
 /**
  * Constructs a new instance of this same class, with roles overridden by the provided `overrides`.
+ * Other than replacing overridden roles, this method is a shallow copy
+ * that reuses role values.
  */
-fun <M : MixData> M.with(vararg overrides: Part): M =
-    this::class.constructFromParts((parts + overrides).toTypedArray())
+fun <M : MixParts> M.with(vararg overrides: Part): M =
+    this::class.constructFromParts(parts + overrides)
 
 /**
  * Returns an instance of this class with the value of `role` mapped through `f`.
  * If the receiver does not have `role`, then `f` is passed `null`. If `f` returns
  * `null`, then the returned instance does not have `role` (which may cause an exception
- * if the receiver's class requires `role`).
+ * if the receiver's class requires `role`). Other than replacing the specified role,
+ * this method is a shallow copy that reuses role values.
  */
-inline fun <M : MixData, reified V : Any> M.mapAt(role: Role<V>, f: (V?) -> V?): M {
+inline fun <M : MixParts, reified V : Any> M.mapAt(role: Role<V>, f: (V?) -> V?): M {
     val oldValue: V? = this[role]
     val newValue: V? = f(oldValue)
     return if (newValue != null) {
         with(Part(role.qualifiedName, newValue))
     } else {
         this::class.constructFromParts(
-            parts.filter { it.keyName != role.qualifiedName }.toTypedArray()
+            parts.filter { it.keyName != role.qualifiedName }
         )
     }
 }
 
-fun <R: Any> KClass<R>.constructFromParts(parts: Array<Part>): R {
+/**
+ * Returns an instance of this class with the value at `path` mapped through `f`.
+ * All path elements except the last must represent `MixParts` values that already
+ * exist, else this method throws `IllegalArgumentException`. If no value is
+ * present at the last path element, then `f` is passed `null`. If `f` returns
+ * `null`, then `role` is omitted from the `Mix` value at the penultimate path
+ * element (which may cause an exception if that `Mix` class requires `role`).
+ *
+ * `path` can be empty, which means the mapping should occur at the root.
+ * In this case, this method throws `IllegalStateException` if `f` returns `null`.
+ *
+ * This method constructs new `MixParts` instances all the way down `path`,
+ * preserving runtime classes. In all other respects, it is a shallow copy
+ * that reuses role values. Beyond the usual cautions about reuse of mutable
+ * values in shallow copies, the complicated mix of shared and new `MixParts`
+ * instances may be problematic for `Remix`s.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <M : MixParts, V : Any> M.mapAt(path: RolePath, f: (V?) -> V?): M =
+    if (path.isEmpty()) {
+        val fReturn = f(this as V) as M?
+        check(fReturn != null)
+        fReturn
+    } else {
+        val firstQn = path.first().qualifiedName
+        val oldRoleValue: Any? = this.valueByQualifiedName[firstQn]
+        val newRoleValue: Any? =
+            if (path.size == 1) {
+                f(oldRoleValue as V?)
+            } else {
+                require(oldRoleValue is MixParts)
+                oldRoleValue.mapAt(path.drop(1), f)
+            }
+        val newParts: List<Part> =
+            if (oldRoleValue != null) {
+                if (newRoleValue != null) {
+                    // Transform existing part.
+                    parts.map {
+                        if (it.keyName == firstQn) {
+                            Part(it.keyName, newRoleValue)
+                        } else {
+                            it
+                        }
+                    }
+                } else {
+                    // Drop existing part.
+                    parts.filter { it.keyName != firstQn }
+                }
+            } else {
+                // We only support an absent existing role in the last path element.
+                require(path.size == 1)
+                if (newRoleValue != null) {
+                    parts + Part(firstQn, newRoleValue)
+                } else {
+                    parts
+                }
+            }
+        this::class.constructFromParts(newParts)
+    }
+
+fun <R : Any> KClass<R>.constructFromParts(parts: List<Part>): R {
     val ctor = primaryConstructor
         ?: throw IllegalArgumentException("$this has no primary constructor")
-    return ctor.call(parts)
+    return ctor.call(parts.toTypedArray())
 }
 
 /**
@@ -58,7 +121,7 @@ fun <R: Any> KClass<R>.constructFromParts(parts: Array<Part>): R {
  * Subclasses can have `val` properties that are delegated to `Role<V>`, in which case those properties
  * will take on the values of the corresponding roles.
  */
-open class Mix(vararg parts: Part) : MixData {
+open class Mix(vararg parts: Part) : MixParts {
 
     override val valueByQualifiedName: Map<String, Any> =
         parts.associate { it.keyName to it.value }
@@ -68,7 +131,7 @@ open class Mix(vararg parts: Part) : MixData {
 
     override fun equals(other: Any?): Boolean =
         other is Mix && other.javaClass == javaClass
-            && other.valueByQualifiedName == valueByQualifiedName
+                && other.valueByQualifiedName == valueByQualifiedName
 
     override fun hashCode(): Int =
         31 * valueByQualifiedName.hashCode() + javaClass.hashCode()
@@ -98,7 +161,7 @@ annotation class RemixMarker
  * the property's function argument.
  */
 @RemixMarker
-open class Remix(vararg parts: Part): MixData {
+open class Remix(vararg parts: Part) : MixParts {
 
     override val valueByQualifiedName: MutableMap<String, Any> =
         parts.associateTo(mutableMapOf()) { it.keyName to it.value }
@@ -111,6 +174,12 @@ open class Remix(vararg parts: Part): MixData {
         }
     }
 
+    /**
+     * Returns this `Remix`'s `Part`s but with top-level `Remix` `Part`s converted
+     * via `Remix.toMix()`.
+     *
+     * FIXME: We should convert `Remix` to `Mix` throughout the tree!
+     */
     protected fun mixParts(): Array<Part> {
         return parts.map { part ->
             if (part.value is Remix) {
@@ -121,6 +190,9 @@ open class Remix(vararg parts: Part): MixData {
         }.toTypedArray()
     }
 
+    /**
+     * Converts this `Remix` and all of its subordinate `Remix`s to `Mix`s.
+     */
     open fun toMix(): Mix = Mix(*mixParts())
 
     operator fun <V> Role<V>.unaryPlus(): RoleRemixDelegateProvider<V?> =
@@ -131,7 +203,7 @@ open class Remix(vararg parts: Part): MixData {
 
     override fun equals(other: Any?): Boolean =
         other is Remix && other.javaClass == javaClass
-            && other.valueByQualifiedName == valueByQualifiedName
+                && other.valueByQualifiedName == valueByQualifiedName
 
     override fun hashCode(): Int =
         31 * valueByQualifiedName.hashCode() + javaClass.hashCode()
@@ -167,7 +239,7 @@ inline operator fun <reified R : Any> MixRoleRemixDelegateProvider<R>.provideDel
     prop: KProperty<*>
 ): MixRoleRemixDelegate<R> {
     return MixRoleRemixDelegate(qualifiedName) {
-        R::class.constructFromParts(arrayOf())
+        R::class.constructFromParts(listOf())
     }
 }
 
@@ -200,12 +272,18 @@ abstract class Role<V> : Named() {
     }
 }
 
+typealias RolePath = List<Role<*>>
+
 class RoleMixDelegate<V>(private val value: V) : ReadOnlyProperty<Mix, V> {
     override operator fun getValue(thisRef: Mix, property: KProperty<*>): V = value
 }
 
 data class Part constructor(val keyName: String, val value: Any)
 
+/**
+ * TODO: Change this name to `has`?
+ * TODO: Provide a counterpart for `MixRole`.
+ */
 infix fun <V : Any> Role<V>.to(value: V): Part {
     return Part(qualifiedName, value)
 }
